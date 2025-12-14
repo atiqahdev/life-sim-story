346ea3a0762a369e3bee6e48932a7b84e1f2ca16
atiqahdev <atiqahzah.dev@gmail.com>
2025-12-14 22:15:09 +0800

    Add files via upload
    Separate templates from logic

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>Life Sim Story</h1>
-    <p>You wake up late for the day.</p>
-    <a href='/rush'>Rush out immediately</a><br><br>
-    <a href='/rest'>Stay in bed a little longer</a>
-    """
+    return render_template("home.html")
 
 @app.route("/rush")
 def rush():
-    return """
-    <p>You rush out. You're tired but on time.</p>
-    <p>Energy: 40</p>
-    <p>Mood: stressed</p>
-    <a href='/'>Start over</a>
-    """
+    return render_template("rush.html")
 
 @app.route("/rest")
 def rest():
-    return """
-    <p>You rest a bit more. You feel calmer, but you're late.</p>
-    <p>Energy: 55</p>
-    <p>Mood: calm</p>
-    <a href='/'>Start over</a>
-    """
+    return render_template("rest.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run()
